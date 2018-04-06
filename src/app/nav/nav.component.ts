59ec7c112f9f0ac6aef8016e5f31c2fe8fe2c0eb
Helen <kirren@live.com>
2018-04-06 11:49:43 +0300

    Add dynamic active class on current page

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  constructor(public location: Location) { }
 
   ngOnInit() {
+  }
+
+  isActive(viewLocation: string): boolean {
+    return this.location.path() === viewLocation;
   }
 
 }
